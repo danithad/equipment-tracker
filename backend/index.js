64af18df01a917b0f0bf278c4b6caf13c4d0ee72
Danitha Dinesh <danithad03@gmail.com>
2025-12-18 01:02:18 +0530

    Configure backend for env variables and deployment

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,18 @@ const cors = require("cors");
 const equipmentRoutes = require("./routes/equipmentRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb+srv://danithad03_db_user:eJVnj26STpnZ04B0@cluster0.iqzgus3.mongodb.net/?appName=Cluster0")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
 
 app.use("/api/equipment", equipmentRoutes);
 
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
